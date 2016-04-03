3e65b596118860b11ac9eedbe67141502c3856cf
kduckets <kmditroia@gmail.com>
2016-04-03 10:15:10 -0700

    focus bug fix

--- src/app/directives/focus.js
+++ src/app/directives/focus.js
@@ -1,22 +1,19 @@
-module.exports =  function($timeout, $parse) {
-  return {
+module.exports =  function($timeout,$parse) {
+   return {
     //scope: true,   // optionally create a child scope
     link: function(scope, element, attrs) {
       var model = $parse(attrs.focus);
       scope.$watch(model, function(value) {
-        console.log('value=',value);
         if(value === true) { 
           $timeout(function() {
             element[0].focus(); 
           });
         }
       });
-      // to address @blesh's comment, set attribute value to 'false'
       // on blur event:
-      element.bind('blur', function() {
-         console.log('blur');
-         scope.$apply(model.assign(scope, false));
-      });
+      // element.bind('blur', function() {
+      //    scope.$apply(model.assign(scope, false));
+      // });
     }
   };
 };
