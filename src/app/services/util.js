54536fcba3e7140aef1c3e8729cc59563b538cb0
David Merritt <dmerritt@fluxxlabs.com>
2016-05-04 22:34:29 -0700

    fix trim util

--- src/app/services/util.js
+++ src/app/services/util.js
@@ -1,6 +1,9 @@
 module.exports = function() {
   var result = {
     trim: function(text, max_chars){
+      if (text.length < max_chars) {
+        return text;
+      }
       return $.trim(text).substring(0, max_chars)
           .split(" ").slice(0, -1).join(" ") + "...";
     }
