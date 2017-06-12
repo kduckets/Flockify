5a2509fa27ccb766fbdad178e34e5db0eaec2163
kduckets <kmditroia@gmail.com>
2017-06-12 13:44:14 -0700

    access token bug

--- server.js
+++ server.js
@@ -113,12 +113,13 @@
 
           // Save the access token so that it's used in future calls
           spotifyApi.setAccessToken(data.body['access_token']);
+          var access_token = spotifyApi.getAccessToken();
+          console.log(access_token);
+          resp.send(access_token);
         }, function(err) {
               console.log('Something went wrong when retrieving an access token', err);
         });
-        var access_token = spotifyApi.getAccessToken();
-        console.log(access_token);
-        resp.send(access_token);
+
       });
 
 
