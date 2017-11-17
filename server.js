b90c44a4eff869a91a75121639a3c033c7f8625e
kduckets <kmditroia@gmail.com>
2017-11-17 15:34:55 -0800

    new number for notification

--- server.js
+++ server.js
@@ -67,7 +67,7 @@
     router.post('/wanotification', function(req, resp){
       var clientId = "kmditroia@gmail.com";
       var clientSecret = "1def0f5ac5a04884b8fd0434e755b884";
-      var phoneNumbers = ["14152508533", "15086330105", "19789734390", "12566588765", "19783282088","19784306872","19789878891","19788521242","15083400792","19148829254","15084796150"];
+      var phoneNumbers = ["14152508533", "15086330105", "19789734390", "12566588765", "19783282088","19784306872","19789878891","19788521242","15083400792","19148829254","15084796150","15083403559"];
       var arrayLength = phoneNumbers.length;
 
       for (var i = 0; i < arrayLength; i++) {
