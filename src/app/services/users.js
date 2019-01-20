06473af9e7df5a5fdf6e8a4bbe860829092a1c63
Kevin DiTroia <kdi@sitecore.net>
2019-01-19 20:52:20 -0500

    Update users.js

--- src/app/services/users.js
+++ src/app/services/users.js
@@ -1,7 +1,7 @@
 module.exports = function($firebaseArray, $firebaseObject, $route, Auth, FIREBASE_URL, $q, $location, $firebaseAuth){
 
-
   var auth = $firebaseAuth();
+  auth.$onAuthStateChanged(function(user) {
   var usersRef = firebase.database().ref('users');
   var users = $firebaseArray(usersRef);
   // var current_group = localStorage.getItem('current_group');
@@ -26,6 +26,32 @@ module.exports = function($firebaseArray, $firebaseObject, $route, Auth, FIREBAS
       });
     });
   }
+})
+
+var usersRef = firebase.database().ref('users');
+var users = $firebaseArray(usersRef);
+// var current_group = localStorage.getItem('current_group');
+var current_group = 'firsttoflock'
+var current_group_name = '';
+var current_user_auth_data = firebase.auth().currentUser;
+var current_user_id = (current_user_auth_data) ? current_user_auth_data.uid : null;
+var current_user_ref = (current_user_auth_data) ? usersRef.child(current_user_id) : null;
+var current_user = (current_user_auth_data) ? $firebaseObject(current_user_ref) : null;
+var subscribed_groups = {'groups': []};
+if(current_user){
+  current_user.$loaded().then(function() {
+    var group_ref =  firebase.database().ref("groups");
+    $.each(Object.keys(current_user.groups), function (idx, group_id) {
+      var group = $firebaseObject(group_ref.child(group_id));
+      group.$loaded().then(function (snapshot) {
+        if (group_id == current_group) {
+          current_group_name = snapshot.group_name;
+        }
+        subscribed_groups.groups.push({id: group_id, group_name: snapshot.group_name})
+      });
+    });
+  });
+}
 
 
   var Users = {
