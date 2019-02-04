712cc6ad3a0cf6ff85cf7740a184e527a4c29e38
Kevin DiTroia <kdi@sitecore.net>
2019-02-04 16:39:55 -0500

    Update concert.js

--- src/app/services/concert.js
+++ src/app/services/concert.js
@@ -6,32 +6,32 @@ module.exports = function ($firebaseArray, $firebaseObject, FIREBASE_URL, Users,
   }
 
   var Concert = {
-    all: concerts,
-
-    add: function (concert, bit_id) {
-      ref.child('concerts').child(user_id).child(bit_id).once("value", function(snapshot) {
-          if(!snapshot.exists()){
-             Notification.add_action(user_id, {
-              url: "/shows/",
-              msg: "Upcoming concert for " + Util.trim(concert.artist_name, 25) + "."
-            });
-    }
-    return ref.child('concerts').child(user_id).child(bit_id).update(concert);
-  })
-    },
-
-    get: function () {
-      return concerts;
-    },
-
-    // get_reference: function(postId){
-    //   return ref.child('posts').child(Users.current_group).child(postId);
-    // },
-
-    delete: function (concert) {
-      // ref.child('concerts').child(user_id).child(concert.post_id).remove();
-      return ref.child('concerts').child(user_id).child(concert.bit_id).update({'removed':true});
-    }
+  //   all: concerts,
+  //
+  //   add: function (concert, bit_id) {
+  //     ref.child('concerts').child(user_id).child(bit_id).once("value", function(snapshot) {
+  //         if(!snapshot.exists()){
+  //            Notification.add_action(user_id, {
+  //             url: "/shows/",
+  //             msg: "Upcoming concert for " + Util.trim(concert.artist_name, 25) + "."
+  //           });
+  //   }
+  //   return ref.child('concerts').child(user_id).child(bit_id).update(concert);
+  // })
+  //   },
+  //
+  //   get: function () {
+  //     return concerts;
+  //   },
+  //
+  //   // get_reference: function(postId){
+  //   //   return ref.child('posts').child(Users.current_group).child(postId);
+  //   // },
+  //
+  //   delete: function (concert) {
+  //     // ref.child('concerts').child(user_id).child(concert.post_id).remove();
+  //     return ref.child('concerts').child(user_id).child(concert.bit_id).update({'removed':true});
+  //   }
   };
   var Concert = {};
   return Concert;
